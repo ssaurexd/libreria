19e4bc8a307c4148a6931b83d3d3ddc07480e0f6
sanjisand <61032413+sanjisand@users.noreply.github.com>
2020-05-24 11:01:58 -0500

    .

--- libreria/settings local.py
+++ libreria/settings local.py
@@ -23,7 +23,7 @@ import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)  
 
 DATABASES['default'].update(db_from_env)  
-STATIC_ROOT = os.path.join(BASE_DIR, 'static') 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') 
 STATIC_URL = '/static/'
 STATICFILES_DIR = os.path.join(BASE_DIR, 'static')
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
