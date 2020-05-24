2a48d39967d29aa32a323c66171727377b996121
sanjisand <61032413+sanjisand@users.noreply.github.com>
2020-05-24 10:39:46 -0500

    .

--- libreria/settings local.py
+++ libreria/settings local.py
@@ -18,9 +18,9 @@ DATABASES = {
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-
 STATIC_URL = '/static/'
 STATICFILES_DIR = os.path.join(BASE_DIR, 'static')
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 #Media
 MEDIA_URL = '/media/'
