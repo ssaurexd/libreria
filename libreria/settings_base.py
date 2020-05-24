ec4108b937e8124531020ed5cc035a6f9d05ad66
sanjisand <61032413+sanjisand@users.noreply.github.com>
2020-05-24 11:16:25 -0500

    Update settings_base.py

--- libreria/settings_base.py
+++ libreria/settings_base.py
@@ -52,7 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+    'whitenoise.storage.CompressedManifestStaticFilesStorage',
 ]
 
 ROOT_URLCONF = 'libreria.urls'
